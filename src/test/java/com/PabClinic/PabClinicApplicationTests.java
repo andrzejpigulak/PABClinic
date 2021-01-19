80835681885e064e601ccb9a2a114e3014cd746b
Szymek <szymkowi.bartek@gmail.com>
2021-01-19 11:09:59 +0100

    Dodaje projekt

---
+++ src/test/java/com/PabClinic/PabClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PabClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PabClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
